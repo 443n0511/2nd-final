9f977c9064f34f14e66da8aa988ea53979583ed3
sample <>
2019-11-22 00:25:20 +0900

    ベンダープレフィックス付与

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const gulp = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
 
+
+const autoprefixer =require('gulp-autoprefixer');  //プラグインの定義
+
+
 // style.scssの監視タスクを作成する
 gulp.task("default", function() {
   // ★ style.scssファイルを監視
@@ -22,6 +26,7 @@ gulp.task("default", function() {
             // (これがないと自動的に止まってしまう)
             .on("error", sass.logError)
         )
+        
         // cssフォルダー以下に保存
         .pipe(gulp.dest("css"))
     );
@@ -29,15 +34,12 @@ gulp.task("default", function() {
 });
 
 
-const autoprefixer =require('gulp-autoprefixer');  //プラグインの定義
-
-
-gulp.task('sass', function () {
+gulp.task("sass", function () {
   return gulp.src("scss/**/**.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({  //autoprefixerの実行
         browsers: ["last 2 versions"],
         cascade: false
-    }))
+    }))   
     .pipe(gulp.dest('css'));
 });
