c26dde26c747b734fd2cdfe7d27aae3c626510ff
sample <>
2019-11-27 14:49:59 +0900

    フォルダ構成を修正

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const autoprefixer =require('gulp-autoprefixer');  //プラグインの定義
 
 
 gulp.task("sass", function () {
-  return gulp.src("scss/**/**.scss")//sassファイルを読み込む
+  return gulp.src("src/scss/**/**.scss")//sassファイルを読み込む
     .pipe(sass().on('error', sass.logError))
    
     .pipe(gulp.dest('css'));//書き出し
@@ -17,13 +17,13 @@ gulp.task("sass", function () {
 // style.scssの監視タスクを作成する
 gulp.task("default", function() {
   // ★ style.scssファイルを監視
-  return gulp.watch("scss/**/**.scss", function() {
+  return gulp.watch("src/scss/**/**.scss", function() {
     // style.scssの更新があった場合の処理
 
     // style.scssファイルを取得
     return (
       gulp
-        .src("scss/style.scss")
+        .src("src/scss/style.scss")
         // Sassのコンパイルを実行
         .pipe(
           sass({
