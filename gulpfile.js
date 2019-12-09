c1ba1058705157d994f208dc2f469a88a01a221d
monaca <>
2019-12-09 15:56:25 +0900

    gulp.fileをtask()を使用しないものに変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,67 @@
-// gulpプラグインの読み込み
-const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
+// gulpプラグインを読み込みます
+const { src, dest, watch, parallel } = require("gulp");
+// Sassをコンパイルするプラグインを読み込みます
 const sass = require("gulp-sass");
- //ベンダープレフィックス
-const autoprefixer =require('gulp-autoprefixer');
+//ベンダープレフィックス
+const autoprefixer = require('gulp-autoprefixer');
 // browser-syncのプラグインの読み込み
 const browserSync = require("browser-sync");
 
 
-/// 自動リロード ////////////////////////////////////////////
-gulp.task("browserSync", function() {
-  browserSync({
-    server: {
-      baseDir : 'dist', // 対象となるディレクトリ
-      index : 'html/index.html', // リロードするHTMLファイル
-    }
-  });
-  // srcフォルダ以下のファイルを監視
-  gulp.watch("src/**", function(done) {
-    browserSync.reload();
-    done(); // ファイルに変更があれば同期しているブラウザをリロード
-  });
-});
+/// sassのコンパイル ////////////////////////////////////////////
+
+const compileSass = () =>
+  // style.scssファイルを取得
+  src("src/scss/**/**.scss")
+    .pipe(sass().on('error', sass.logError))
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "expanded" //CSSの出力形式
+      })
+    )
+    .pipe(autoprefixer({ cascade: false }))
+    // cssフォルダー以下に保存
+    .pipe(dest("src/css"))
+
 /// ファイルをdistにコピー ////////////////////////////////////////////
 
-gulp.task('html', function () {
-  return gulp.src("src/**/*.html")
-    .pipe(gulp.dest('dist/'))
-})
+const html = () =>
+  src("src/**/*.html")
+    .pipe(browserSync.reload({ stream: true })) 
+    .pipe(dest('dist/'))
 
-gulp.task('css', function () {
-  return gulp.src("src/**/*.css")
-    .pipe(gulp.dest('dist/'))
-})
-gulp.task('js', function () {
-  return gulp.src("src/**/*.js")
-    .pipe(gulp.dest('dist/'))
-})
+const css = () =>
+  src("src/**/*.css")
+    .pipe(browserSync.reload({ stream: true })) 
+    .pipe(dest('dist/'))
 
 
+const js = () =>
+  src("src/**/*.js")
+    .pipe(browserSync.reload({ stream: true })) 
+    .pipe(dest('dist/'))
 
 
-/// cssコンパイル ////////////////////////////////////////////
-gulp.task('sass', function() {
-  return gulp.src("src/scss/**/**.scss")//sassファイルを読み込む
-        .pipe(sass().on('error', sass.logError))
-        .pipe( sass({ outputStyle: 'expanded' }) )//CSSの出力形式
-        .pipe(autoprefixer( {cascade:false}))
-        .pipe(gulp.dest("src/css"));//書き出し
-});
+/// 監視 ////////////////////////////////////////////
 
-/// 監視フォルダ ////////////////////////////////////////////
-gulp.task('watch', function(){
-  gulp.watch('src/scss/**/**.scss', gulp.task('sass'));
-  gulp.watch('src/**/*.html', gulp.task('html'));
-  gulp.watch('src/**/*.css', gulp.task('css'));
-  gulp.watch('src/**/*.js', gulp.task('js'));
-  
-});
+const watchSassFiles = () =>
+watch("src/**/**/**.scss", compileSass)
+watch('src/**/*.html', html)
+watch('src/**/*.css', css)
+watch('src/**/*.js', js)
 
-/// Gulpコマンドで動かすもの ////////////////////////////////////////////
 
-gulp.task('default', gulp.series(
-  gulp.parallel('watch','browserSync')
-));
+/// ファイルをdistにコピー ////////////////////////////////////////////
+
+const browsersync = () =>
+  browserSync({
+    server: {
+      baseDir: 'dist', // 対象となるディレクトリ
+      index: 'html/index.html', // リロードするHTMLファイル
+    }
+  })
+
+// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
+exports.default = parallel(watchSassFiles,browsersync)
